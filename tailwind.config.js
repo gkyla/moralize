1bd28ccd7f99341c0bd0cdc60fc620c054c293a8
gitkyla <agitsyang@gmail.com>
2021-01-15 02:11:56 +0700

    add extra breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    screens: {
+      xsm: "420px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px"
+    }
   },
   variants: {
     extend: {}
