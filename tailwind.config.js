652134de3cdece787d281194b7f26700ccbe0343
gitkyla <agitsyang@gmail.com>
2021-01-23 05:20:20 +0700

    change settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      xsm: "420px",
+      xsm: "370px",
       sm: "640px",
       // => @media (min-width: 640px) { ... }
 
