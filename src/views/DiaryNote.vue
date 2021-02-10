121919739633ad22ca21f729385624aaa430b3ae
gitkyla <agitsyang@gmail.com>
2021-02-11 01:49:41 +0700

    add  advanced search modal

--- src/views/DiaryNote.vue
+++ src/views/DiaryNote.vue
@@ -10,50 +10,20 @@
           class="lg:ml-auto flex items-center justify-between  flex-wrap gap-2"
         >
           <div class="flex flex-col items-center">
-            Sort By
-            <select name="Sort By" class="custom-select">
-              <option selected>Default </option>
+            <select name="Sort By" class="custom-select rounded-full">
+              <option value="none" selected>None</option>
+              <option>Default </option>
             </select>
           </div>
-          <div class="flex flex-col items-center">
-            Mood
-            <select name="mood" class="custom-select" v-model="select.mood">
-              <option
-                v-for="mood in filteredGettersOptions.mood"
-                :value="mood"
-                :key="mood"
-              >
-                {{ mood }}
-              </option>
-            </select>
-          </div>
-          <div class="flex flex-col items-center">
-            Location
-            <select
-              name="location"
-              class="custom-select"
-              v-model="select.location"
+
+          <div class=" flex items-center">
+            <button
+              class="box-atention-color rounded-2xl p-3 font-bold"
+              @click="openAdvancedSearch = true"
             >
-              <option
-                v-for="location in filteredGettersOptions.location"
-                :value="location"
-                :key="location"
-              >
-                {{ location }}
-              </option>
-            </select>
-          </div>
-          <div class="flex flex-col items-center">
-            Tags
-            <select name="tags" class="custom-select" v-model="select.tag">
-              <option
-                v-for="tag in filteredGettersOptions.tag"
-                :value="tag"
-                :key="tag"
-              >
-                {{ tag }}
-              </option>
-            </select>
+              Advanced Search
+              <font-awesome-icon class="ml-3" icon="search" />
+            </button>
           </div>
         </div>
       </div>
@@ -62,7 +32,9 @@
       id="list-diary"
       class=" bg-white rounded-xl bg-opacity-70 shadow-md lg:p-5 p-2 my-4 lg:my-8"
     >
-      <the-title :title="defineTitle" :amount="defineTitleAmount"></the-title>
+      <div class="flex items-center justify-between">
+        <the-title :title="defineTitle" :amount="defineTitleAmount"></the-title>
+      </div>
       <div class="mt-6 grid grid-cols-1 bsm:grid-cols-2 xl:grid-cols-3 gap-3">
         <div v-if="diaryState.loading">Loading up data ..</div>
         <card-diary
@@ -81,6 +53,96 @@
         ></card-diary>
       </div>
     </section>
+
+    <teleport to="#modal">
+      <transition name="fade" mode="out-in">
+        <div
+          id="modal-box"
+          class="fixed flex items-start justify-center z-50 top-0 left-0 bottom-0 right-0 bg-gray-400 bg-opacity-50 md:p-4 p-2 xl:px-72 xl:py-16"
+          v-if="openAdvancedSearch"
+        >
+          <div
+            id="modal-content"
+            class="relative block bg-white w-screen md:w-96 h-auto max-h-full shadow-lg rounded-2xl overflow-hidden "
+          >
+            <div
+              id="window-control"
+              class="box-atention-color w-full h-auto p-2 flex"
+            >
+              <button
+                class="nav-link mb-0 bg-gray-600 text-white hover:bg-gray-400 ml-auto"
+                @click="openAdvancedSearch = false"
+              >
+                <font-awesome-icon class="text-2xl text-white" icon="times" />
+              </button>
+            </div>
+            <div
+              id="modal-info"
+              class="md:mt-0 w-full h-full overflow-y-auto overflow-x-hidden px-5 py-2"
+            >
+              <div class="grid grid-cols-1">
+                <div class="flex flex-col">
+                  Mood
+                  <select
+                    name="mood"
+                    class="custom-select"
+                    v-model="select.mood"
+                  >
+                    <option value="None" selected>None</option>
+                    <option
+                      v-for="mood in filteredGettersOptions.mood"
+                      :value="mood"
+                      :key="mood"
+                    >
+                      {{ mood }}
+                    </option>
+                  </select>
+                </div>
+                <div class="flex flex-col">
+                  Location
+                  <select
+                    name="location"
+                    class="custom-select"
+                    v-model="select.location"
+                  >
+                    <option value="None" selected>None</option>
+                    <option
+                      v-for="location in filteredGettersOptions.location"
+                      :value="location"
+                      :key="location"
+                    >
+                      {{ location }}
+                    </option>
+                  </select>
+                </div>
+                <div class="flex flex-col">
+                  Tags
+                  <select
+                    name="tags"
+                    class="custom-select"
+                    v-model="select.tag"
+                  >
+                    <option value="None" selected>None</option>
+                    <option
+                      v-for="tag in filteredGettersOptions.tag"
+                      :value="tag"
+                      :key="tag"
+                    >
+                      {{ tag }}
+                    </option>
+                  </select>
+                </div>
+                <button
+                  class="bg-gray-600 px-4 py-2 rounded-full text-white font-bold my-5  max-w-max ml-auto"
+                >
+                  Search
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
+    </teleport>
   </div>
 </template>
 
@@ -103,6 +165,7 @@ export default {
     const currentFiltered = computed(
       () => store.state.diary.currentResultFilteredOption
     );
+    const openAdvancedSearch = ref(false);
     // const pinedDiary = computed(() => store.getters["diary/getPinedDiary"]);
     const getDiary = computed(() => store.getters["diary/searchDiary"]);
     const allDiaryState = computed(() => store.state.diary.allDiary);
@@ -117,39 +180,18 @@ export default {
 
     // Select Ref
     const select = reactive({
-      mood: "",
-      location: "",
-      tag: ""
+      mood: "None",
+      location: "None",
+      tag: "None"
     });
 
     // For now only work 2 tag ( mood & location)
     const selectFiltered = computed(() => {
-      let mood = select.mood;
-      let location = select.location;
-      let tag = select.tag;
-
-      const res = allDiaryState.value.filter(diary => {
-        let filtered = true;
-        if (mood && mood.length > 0) {
-          userSelectSomething.value = true;
-          filtered = diary.mood == mood;
-        }
-
-        if (filtered) {
-          if (location && location.length > 0) {
-            userSelectSomething.value = true;
-            filtered = diary.location == location;
-          }
-
-          if (tag && tag.length > 0) {
-            userSelectSomething.value = true;
-            filtered = diary.tag == tag;
-          }
-        }
-        return filtered;
+      const tempSearchTag = reactive({
+        mood: select.mood,
+        location: select.location,
+        tag: select.tag
       });
-      console.log(res);
-      return res;
     });
     store.commit("diary/setCurrentResultFilteredOption", selectFiltered);
 
@@ -180,7 +222,8 @@ export default {
       filteredGettersOptions,
       select,
       selectFiltered,
-      currentFiltered
+      currentFiltered,
+      openAdvancedSearch
     };
   }
 };
