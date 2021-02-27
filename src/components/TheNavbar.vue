26193ea17ec5b8f506b6880b083afcae0917c31f
gitkyla <agitsyang@gmail.com>
2021-02-27 19:02:34 +0700

    update navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,29 +1,29 @@
 <template>
   <nav
-    class="mt-auto w-full md:relative md:w-28 lg:w-64  md:h-screen flex items-center p-4 md:flex-col nav-color flex-shrink-0 "
+    class="mt-auto w-full md:relative md:w-28 lg:w-64  md:h-screen flex items-center px-1 lg:px-4 md:flex-col nav-color flex-shrink-0 "
   >
     <!-- <user-badge class="hidden md:block"></user-badge> -->
     <div id="nav-links" class="text-left flex justify-around w-full md:block">
       <router-link to="/" class="nav-link font-bold">
         <font-awesome-icon class="text-2xl" icon="home" />
-        <p class=" ml-5 hidden lg:inline">Home</p>
+        <p class=" ml-5 hidden  lg:inline-block">Home</p>
       </router-link>
       <router-link to="/diary-note" class="nav-link font-bold">
         <font-awesome-icon class="text-2xl" icon="book-open" />
-        <p class=" ml-5 hidden lg:inline">Diary Note</p>
+        <p class=" ml-5 hidden lg:inline-block">Diary Note</p>
       </router-link>
       <add-button-handler class="md:hidden" />
       <router-link to="/your-target" class="nav-link font-bold">
         <font-awesome-icon class="text-2xl" icon="sticky-note" />
-        <p class=" ml-5 hidden lg:inline">Your Target</p>
+        <p class=" ml-5 hidden lg:inline-block">Your Target</p>
       </router-link>
       <router-link to="/event" class="nav-link font-bold">
         <font-awesome-icon class="text-2xl" icon="calendar-alt" />
-        <p class=" ml-5 hidden lg:inline">Event</p>
+        <p class=" ml-5 hidden lg:inline-block">Event</p>
       </router-link>
       <router-link to="/pined" class="nav-link font-bold hidden lg:block">
         <font-awesome-icon class="text-2xl" icon="thumbtack" />
-        <p class=" ml-5 hidden lg:inline">Pined</p>
+        <p class=" ml-5 hidden lg:inline-block">Pined</p>
       </router-link>
     </div>
   </nav>
@@ -53,6 +53,9 @@ export default {
       return require("../assets/" + logo);
     }
 
+    // Media query js
+    // hover nav -> tulisan nya expand ( berlaku di md breakpoint )
+
     return {
       getImg,
       links
@@ -63,7 +66,7 @@ export default {
 
 <style>
 .nav-link {
-  @apply block py-2 sm:py-4 px-4 sm:px-6 md:flex md:items-center hover:bg-gray-200 transition-all rounded-md md:mb-5;
+  @apply block py-2 my-3 sm:py-4 px-4 sm:px-6 md:flex md:items-center justify-center md:justify-start hover:bg-gray-200 transition-all rounded-md md:mb-5;
 }
 
 .nav-link.router-link-exact-active {
