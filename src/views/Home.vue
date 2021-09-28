a55a4909b1ac1f9af5171fe624f54c9caaf9c232
gitkyla <agitsyang@gmail.com>
2021-09-28 22:21:41 +0700

    add greetingMsg to be relative to the current time ⏰

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,48 @@
 <template>
   <div id="home" class="container-page">
-    <div id="group-top" class="xl:flex xl:gap-5 ">
+    <div id="group-top" class="xl:flex xl:gap-5">
       <div
         id="welcome"
-        class="flex items-center nav-color p-5 shadow-md rounded-md w-full xl:w-1/3"
+        class="
+          flex
+          items-center
+          nav-color
+          p-5
+          shadow-md
+          rounded-md
+          w-full
+          xl:w-1/3
+        "
       >
         <div class="w-full h-full">
           <h1 class="font-bold text-xl sm:text-2xl md:text-4xl mb-2">
-            Good Morning
+            {{ greetingsMsg }}
           </h1>
           <span class="text-sm sm:text-lg">
-            {{ greetings ? greetings : "Setting up time .." }}</span
+            {{
+              timeTick
+                ? timeTick.format("MMMM Do YYYY, hh:mm:ss A")
+                : "Setting up time .."
+            }}</span
           >
         </div>
       </div>
-      <div id="group-2" class="flex-grow my-4 xl:my-0 shadow-md ">
+      <div id="group-2" class="flex-grow my-4 xl:my-0 shadow-md">
         <div class="box-atention-color h-full rounded-md p-5 text-left">
           <h1 id="user-data" class="font-bold text-xl lg:text-2xl">Info</h1>
           <div
             id="count-collection"
-            class="flex items-center justify-evenly shadow-lg bg-gray-100 py-1 lg:py-3 rounded-lg mt-2"
+            class="
+              flex
+              items-center
+              justify-evenly
+              shadow-lg
+              bg-gray-100
+              py-1
+              lg:py-3
+              rounded-lg
+              mt-2
+            "
           >
             <div
               id="count-diary"
@@ -51,15 +74,56 @@
     </div>
 
     <div
-      class="grid grid-cols-1 xl:grid-cols-2 gap-5 lg:shadow-md bg-white rounded-xl bg-opacity-70 lg:p-5 lg:my-8"
+      class="
+        grid grid-cols-1
+        gap-5
+        lg:shadow-md
+        bg-white
+        rounded-xl
+        bg-opacity-70
+        lg:p-5
+        lg:my-8
+      "
     >
-      <div class="grid grid-cols-1 lg:grid-cols-2   gap-4 col-span-1">
+      <div class="flex p-2">
+        <div class="bg-white shadow-xl p-10">
+          <h1
+            class="
+              font-bold
+              text-center
+              my-5
+              text-xl
+              py-3
+              px-5
+              rounded-full
+              box-atention-color
+              text-gray-600
+            "
+          >
+            Diary Stats
+          </h1>
+          <p class="stats-information">
+            First Time Created On :
+            <span class="stats-value"> 12 Januari 2020</span>
+          </p>
+          <p class="stats-information">
+            Total Of pined Diary : <span class="stats-value"> 10 </span>
+          </p>
+          <p class="stats-information">Recent Created : 20 June 2021</p>
+          <p class="stats-information">Total Diary : 40</p>
+          <p class="stats-information">
+            Total of Various Location : 11 Location
+          </p>
+          <p class="stats-information">Total of tag : 13 Tag</p>
+        </div>
+      </div>
+      <div class="grid grid-cols-1 lg:grid-cols-4 gap-4 col-span-1">
         <div
           name="Recent Diary"
-          class=" rounded-xl mt-5 grid grid-cols-1 gap-4 col-span-2 "
+          class="rounded-xl mt-5 grid grid-cols-1 gap-4 col-span-2"
         >
           <h1 class="font-bold text-lg">Your Recent Diary</h1>
-          <div class="grid grid-cols-1  bsm:grid-cols-2 gap-5">
+          <div class="grid grid-cols-1 bsm:grid-cols-2 gap-5">
             <div v-if="diaryState.loading">Loading up data ..</div>
             <div v-else-if="isDiaryEmpty">
               <placeholder-card></placeholder-card>
@@ -73,64 +137,36 @@
             />
           </div>
         </div>
-        <div class="col-span-2">
-          <h1 class="font-bold text-lg mb-5 ">Most Active Diary</h1>
-          <card-diary
-            v-if="!diaryState.loading && !isDiaryEmpty"
-            :diary="diaryState.allDiary[diaryState.allDiary.length - 1]"
-            :customClass="['grid-diary-item']"
-          />
-          <placeholder-card v-if="isDiaryEmpty"></placeholder-card>
-        </div>
-      </div>
-      <div class="flex p-2 justify-center items-center">
-        <div class="bg-white shadow-xl p-10">
-          <h1
-            class="font-bold text-center my-5 text-xl py-3 px-5 rounded-full box-atention-color text-gray-600"
-          >
-            Diary Stats
-          </h1>
-          <p class="stats-information">
-            First Time Created On :
-            <span class="stats-value"> 12 Januari 2020</span>
-          </p>
-          <p class="stats-information">
-            Total Of pined Diary : <span class="stats-value"> 10 </span>
-          </p>
-          <p class="stats-information">
-            Recent Created : 20 June 2021
-          </p>
-          <p class="stats-information">Total Diary : 40</p>
-          <p class="stats-information">
-            Total of Various Location : 11 Location
-          </p>
-          <p class="stats-information">Total of tag : 13 Tag</p>
-        </div>
       </div>
     </div>
 
     <!-- Target -->
     <div
-      class="grid grid-cols-1 xl:grid-cols-2 gap-5 lg:shadow-md bg-white rounded-xl bg-opacity-70 lg:p-5 lg:my-8"
+      class="
+        grid grid-cols-1
+        gap-5
+        lg:shadow-md
+        bg-white
+        rounded-xl
+        bg-opacity-70
+        lg:p-5
+        lg:my-8
+      "
     >
-      <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 col-span-1">
-        <div class=" rounded-xl mt-5 grid grid-cols-1 gap-4 col-span-2 ">
-          <h1 class="font-bold text-lg">Recent Target</h1>
-          <div class="grid grid-cols-1  xsm:grid-cols-2 gap-5">
-            <card-target :done="false" />
-          </div>
-        </div>
-        <div class=" col-span-2">
-          <div>
-            <h1 class="font-bold text-lg mb-5 ">Recent Finished</h1>
-            <card-target :done="true" />
-          </div>
-        </div>
-      </div>
-      <div class="flex justify-center items-center ">
+      <div class="flex">
         <div class="bg-white shadow-xl p-10">
           <h1
-            class="font-bold text-center my-5 text-xl py-3 px-5 rounded-full target-color text-gray-600"
+            class="
+              font-bold
+              text-center
+              my-5
+              text-xl
+              py-3
+              px-5
+              rounded-full
+              target-color
+              text-gray-600
+            "
           >
             Target Stats
           </h1>
@@ -141,10 +177,17 @@
           <p class="stats-information">
             Total Target : <span class="stats-value"> 10 </span>
           </p>
-          <p class="stats-information">
-            Recent Created : 20 June 2021
-          </p>
+          <p class="stats-information">Recent Created : 20 June 2021</p>
           <p class="stats-information">Total Finished Target : 40</p>
+        </div>
+      </div>
+
+      <div class="grid grid-cols-1 lg:grid-cols-4 gap-4 col-span-1">
+        <div class="rounded-xl mt-5 grid grid-cols-1 gap-4 col-span-2">
+          <h1 class="font-bold text-lg">Recent Target</h1>
+          <div class="grid grid-cols-1 xsm:grid-cols-2 gap-5">
+            <card-target :done="false" />
+          </div>
         </div>
       </div>
     </div>
@@ -167,7 +210,7 @@ export default {
   components: {
     CardDiary,
     CardTarget,
-    PlaceholderCard
+    PlaceholderCard,
     // itemContainer
   },
 
@@ -197,22 +240,33 @@ export default {
       }
     }
 
-    const greetings = ref(null);
+    const timeTick = ref(null);
+    const greetingsMsg = ref(null);
+
+    function calculateTime() {
+      const momentHour = moment().hour();
+      return momentHour > 21
+        ? "Good Night"
+        : momentHour > 5
+        ? "Good Morning"
+        : "Good Afternoon";
+    }
 
     // Update every second
-    setInterval(
-      () => (greetings.value = moment().format("MMMM Do YYYY, hh:mm:ss A")),
-      1000
-    );
+    setInterval(() => {
+      timeTick.value = moment();
+      greetingsMsg.value = calculateTime();
+    }, 1000);
 
     return {
       user,
-      greetings,
+      timeTick,
       getFirstTwoLast,
       // pinState
+      greetingsMsg,
       isDiaryEmpty,
-      diaryState
+      diaryState,
     };
-  }
+  },
 };
 </script>
