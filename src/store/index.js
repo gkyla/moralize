a86335fbfaf2043cb1a72bfafed50e32dadff93d
gitkyla <agitsyang@gmail.com>
2021-10-20 23:14:56 +0700

    add calenderEvent to the modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import user from "./user-info.js";
 import diary from "./user-diary.js";
+import calenderEvent from "./user-event.js";
 
 export default createStore({
   modules: {
     user,
-    diary
+    diary,
+    calenderEvent
   }
 });
