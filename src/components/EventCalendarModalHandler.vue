49f36e7ff3b31aa595ced8298501e87cae6f31a8
gitkyla <agitsyang@gmail.com>
2021-11-03 00:21:03 +0700

    fix update but with the wrong way to do it 😫

--- src/components/EventCalendarModalHandler.vue
+++ src/components/EventCalendarModalHandler.vue
@@ -17,7 +17,15 @@
   >
     <div class="bg-white text-center p-56 shadow-md relative z-50" @click.stop>
       <h1 class="text-2xl">test</h1>
-      <h1 class="text-2xl">{{ eventObj.event.title }}</h1>
+      <h1 class="text-2xl">{{ editableEvent.title }}</h1>
+
+      <form
+        @submit.prevent="updateEvent"
+        class="border border-black border-b-2"
+      >
+        <input type="text" v-model="editableEvent.title" />
+      </form>
+
       <button @click.stop="closeModal">Close</button>
       <button @click.stop="deleteEvent">Delete Label Event</button>
     </div>
@@ -25,7 +33,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive, computed, onUnmounted } from "vue";
 import { useStore } from "vuex";
 // import { toRefs } from '@vue/reactivity';
 
@@ -39,10 +47,39 @@ export default {
   setup(props, { emit }) {
     const modalIsOpened = ref(true);
     const store = useStore();
+    const eventsData = computed(() => store.state.calenderEvent.events);
+
+    const copyEventObjProps = computed(() =>
+      eventsData.value.find(
+        (_event) =>
+          _event.id == props.eventObj.event.id &&
+          _event.title == props.eventObj.event.title
+      )
+    );
+    const currentIndexOnTheArray = computed(() =>
+      eventsData.value.findIndex(
+        (_event) =>
+          _event.id == props.eventObj.event.id &&
+          _event.title == props.eventObj.event.title
+      )
+    );
+
+    const editableEvent = reactive({
+      title: copyEventObjProps.value.title,
+      start: copyEventObjProps.value.start,
+      end: copyEventObjProps.value.end,
+    });
 
     function closeModal() {
       modalIsOpened.value = false;
       emit("modalStatus", modalIsOpened.value);
+    }
+
+    function updateEvent() {
+      eventsData.value[currentIndexOnTheArray.value] = {
+        ...eventsData.value[currentIndexOnTheArray.value],
+        title: editableEvent.title,
+      };
     }
 
     function deleteEvent() {
@@ -54,7 +91,11 @@ export default {
       emit("modalStatus", modalIsOpened.value);
     }
 
-    return { closeModal, deleteEvent };
+    onUnmounted(() => {
+      editableEvent.value = props.eventObj;
+    });
+
+    return { closeModal, deleteEvent, editableEvent, updateEvent };
   },
 };
 </script>
