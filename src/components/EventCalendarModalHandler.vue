615ff63667e0e590111bcb24f1e0dbb64751c36c
gitkyla <agitsyang@gmail.com>
2021-11-04 23:03:28 +0700

    update ui modal, add start & end input

--- src/components/EventCalendarModalHandler.vue
+++ src/components/EventCalendarModalHandler.vue
@@ -27,7 +27,11 @@
       "
       @click.stop
     >
-      <div id="top-modal" class="flex p-4 box-atention-color w-full">
+      <div
+        id="top-modal"
+        class="flex p-4 box-atention-color w-full items-center"
+      >
+        <h1 class="text-2xl font-bold">Event Editor</h1>
         <button
           @click.stop="closeModal"
           class="ml-auto bg-gray-600 text-white px-3 py-1 rounded-lg"
@@ -37,54 +41,77 @@
       </div>
       <div id="inner-modal" class="p-4 w-full h-full">
         <form @submit.prevent="updateEvent" class="rounded-md">
-          <div>
-            <p class="ml-2">-- Title</p>
-            <input
-              type="text"
-              class="w-full border-2 rounded-md p-2"
-              v-model="editableEvent.title"
-            />
+          <div id="input-group">
+            <div>
+              <p class="ml-2">-- Title</p>
+              <input
+                type="text"
+                class="w-full border-2 rounded-md p-2"
+                v-model="editableEvent.title"
+              />
+            </div>
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-1 mt-2">
+              <div>
+                <p class="ml-2">-- Start</p>
+
+                <input
+                  type="date"
+                  class="w-full border-2 rounded-md p-2"
+                  v-model="editableEvent.start"
+                />
+              </div>
+              <div>
+                <p class="ml-2">-- End</p>
+                <input
+                  type="date"
+                  class="w-full border-2 rounded-md p-2"
+                  v-model="editableEvent.end"
+                />
+              </div>
+            </div>
           </div>
         </form>
 
-        <button
-          @click.stop="deleteEvent"
-          class="
-            box-atention-color
-            px-4
-            py-1
-            rounded-lg
-            mt-5
-            mr-2
-            hover:bg-red-500
-            transition-all
-          "
-        >
-          Remove
-        </button>
-        <button
-          type="submit"
-          class="box-atention-color px-4 py-1 rounded-lg mt-5"
-          @click.stop="updateEvent"
-        >
-          Save
-        </button>
+        <div class="flex">
+          <button
+            @click.stop="deleteEvent"
+            class="
+              ml-auto
+              box-atention-color
+              px-4
+              py-1
+              rounded-lg
+              mt-5
+              mr-2
+              hover:bg-red-500
+              transition-all
+            "
+          >
+            Remove
+          </button>
+          <button
+            type="submit"
+            class="box-atention-color px-4 py-1 rounded-lg mt-5"
+            @click.stop="updateEvent"
+          >
+            Save
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive, computed, onUnmounted } from "vue";
+import { ref, reactive, computed } from "vue";
 import { useStore } from "vuex";
-// import { toRefs } from '@vue/reactivity';
 
 export default {
   props: {
     eventObj: {
       type: Object,
-      required: true,
-    },
+      required: true
+    }
   },
   setup(props, { emit }) {
     const modalIsOpened = ref(true);
@@ -93,14 +120,14 @@ export default {
 
     const copyEventObjProps = computed(() =>
       eventsData.value.find(
-        (_event) =>
+        _event =>
           _event.id == props.eventObj.event.id &&
           _event.title == props.eventObj.event.title
       )
     );
     const currentIndexOnTheArray = computed(() =>
       eventsData.value.findIndex(
-        (_event) =>
+        _event =>
           _event.id == props.eventObj.event.id &&
           _event.title == props.eventObj.event.title
       )
@@ -109,7 +136,7 @@ export default {
     const editableEvent = reactive({
       title: copyEventObjProps.value.title,
       start: copyEventObjProps.value.start,
-      end: copyEventObjProps.value.end,
+      end: copyEventObjProps.value.end
     });
 
     function closeModal() {
@@ -121,24 +148,24 @@ export default {
       eventsData.value[currentIndexOnTheArray.value] = {
         ...eventsData.value[currentIndexOnTheArray.value],
         title: editableEvent.title,
+        start: editableEvent.start,
+        end: editableEvent.end
       };
+
+      console.log("updated");
     }
 
     function deleteEvent() {
       store.commit("calenderEvent/deleteEvent", {
-        eventObj: props.eventObj,
+        eventObj: props.eventObj
       });
 
       modalIsOpened.value = false;
       emit("modalStatus", modalIsOpened.value);
     }
 
-    onUnmounted(() => {
-      editableEvent.value = props.eventObj;
-    });
-
     return { closeModal, deleteEvent, editableEvent, updateEvent };
-  },
+  }
 };
 </script>
 
