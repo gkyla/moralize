d868beb8aa84fb3e00aa8d972e39e823e7a01000
gitkyla <agitsyang@gmail.com>
2021-11-05 23:43:43 +0700

    update rule ♐

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,10 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-unused-vars" : 'warn',
+    "no-loss-of-precision" : 'off',
+    "no-nonoctal-decimal-escape" : "off",
+    'no-unsafe-optional-chaining' : 'off',
+    'no-useless-backreference' : 'off',
+    "comma-dangle": 'never'
   }
 };
