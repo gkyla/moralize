8287eefdbbc59b8cd8e04c7f39d936ba59c62285
gitkyla <agitsyang@gmail.com>
2021-11-28 04:49:04 +0700

    add display to the index modules vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from "vuex";
 import user from "./user-info.js";
 import diary from "./user-diary.js";
 import calenderEvent from "./user-event.js";
+import display from "./display-control.js";
 
 export default createStore({
   modules: {
     user,
     diary,
-    calenderEvent
+    calenderEvent,
+    display
   }
 });
